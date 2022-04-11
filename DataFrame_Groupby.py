38707a726385acce3bcb98ea92d1e35a31fee49d
virginie.desharnais <virginie.desharnais@Digital-Grenoble.local>
2022-04-11 16:16:33 +0200

    Modification VBD - variable sortRatingsField

--- DataFrame_Groupby.py
+++ DataFrame_Groupby.py
@@ -60,7 +60,7 @@ print('\n==================================================================\n')
 
 
 # Sort data ratings by created field (groupby + lambda function + sorted)
-sortRatingsField = cloneDF(mergeRatings)
-sortRatingsField = sortRatingsField.groupby(['movie_id', 'title'])['rating'].agg(
-    COUNT=np.size, myAVG=lambda x: x.sum() / float(x.count())).sort('COUNT', ascending=False)
-print('My info sorted: \n%s' % sortRatingsField[:15])
+sortRatingsField_vbd = cloneDF(mergeRatings)
+sortRatingsField_vbd = sortRatingsField_vbd.groupby(['movie_id', 'title'])['rating'].agg(
+    COUNT=np.size, myAVG=lambda x: x.sum() / float(x.count())).sort_values('COUNT', ascending=False)
+print('My info sorted: \n%s' % sortRatingsField_vbd[:15])
